a262fcd24c954c4092d07776a4ea9f702983edae
denphi <denphi@denphi.com>
2019-05-02 11:00:17 -0400

    Added revision for sklearn patch

--- floatview/_version.py
+++ floatview/_version.py
@@ -4,7 +4,7 @@
 # Copyright (c) Project Jupyter.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 9)
+version_info = (0, 1, 10)
 __version__ = ".".join(map(str, version_info))
 
 # The version of the attribute spec that this package
